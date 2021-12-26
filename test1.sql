6aefc5fff4096eeef10e860a1686e345a012a625
ashwini-balu <94046261+ashwini-balu@users.noreply.github.com>
2021-12-26 13:15:31 +0530

    Add files via upload

---
+++ test1.sql
@@ -0,0 +1,5 @@
+create database test1;
+drop database test1;
+use  test1;
+create table register3(id int primary key auto_increment,name varchar(255),password varchar(255),email varchar(255),sex varchar(100),country varchar(255));
+select * from register3;
